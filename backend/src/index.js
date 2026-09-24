bd2ad04e7a103a6d074ef4cbd4bf8992d29d0067
Liudmyla Valier <liudmyla.valier@medieinstitutet.se>
2026-09-24 21:22:33 +0200

    Add completed todo state

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-﻿import express from 'express'
+import express from 'express'
 import cors from 'cors'
 
 const app = express()
@@ -11,12 +11,24 @@ const todoLists = {
   '0000000001': {
     id: '0000000001',
     title: 'First List',
-    todos: ['First todo of first list!'],
+    todos: [
+      {
+        id: 'todo-1',
+        text: 'First todo of first list!',
+        completed: false,
+      },
+    ],
   },
   '0000000002': {
     id: '0000000002',
     title: 'Second List',
-    todos: ['First todo of second list!'],
+    todos: [
+      {
+        id: 'todo-2',
+        text: 'First todo of second list!',
+        completed: false,
+      },
+    ],
   },
 }
 
